88cca72b0b794dd06edadaf84f2014de29a8ba4b
Hugo Wong-Berard <hugowong71@gmail.com>
2020-07-15 17:21:47 +0100

    font

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import "../App.css"
 export default class Header extends Component {
   render() {
     return (
-      <div style={{ textAlign: "center", fontFamily: "Roboto Mono" }}>
+      <div style={{ textAlign: "center", fontFamily: "Poppins" }}>
         <h1>Linear Regression Visualizer</h1>
         <h3>
           <a
